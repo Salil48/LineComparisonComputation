de80ea5827cb8445ad35b2a68e88ce5fe6b19f8b
“Salil48” <salilchharia@gmail.com>
2023-01-01 19:05:39 +0530

    Line Length Comparison using Class Method and Class Variable

---
+++ LineComparison.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputation
+{
+    public class LineComparison
+    {
+        public void Check()
+        {
+            Console.WriteLine("Enter value of x1 : ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of x2 : ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of y1 : ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of y2 : ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of x3 : ");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of x4 : ");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of y3 : ");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of y4 : ");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+            var distance1 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(distance1);
+
+            var distance2 = Math.Sqrt((Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2)));
+            Console.WriteLine(distance2);
+
+            if (distance1 > distance2)
+            {
+                Console.WriteLine("The Line1 Is Greater Than Line2");
+            }
+            else if (distance1 < distance2)
+            {
+                Console.WriteLine("The Line1 Is Smaller Than Line2");
+            }
+            else
+            {
+                Console.WriteLine("Both The Lines Are Equal");
+            }
+        }
+    }
+}
